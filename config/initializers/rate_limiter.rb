37c193f5bdd545c337fdfa0f5370cc5d2846610c
mdkalish <mdkalish4git@gmail.com>
2015-03-25 18:41:41 +0100

    Use Dalli - attempt 3

--- config/initializers/rate_limiter.rb
+++ config/initializers/rate_limiter.rb
@@ -1,3 +1,5 @@
 if Rails.env.production?
   Rails.application.configure { |c| c.middleware.use RateLimiter, store: Rails.cache.dalli }
+else
+  Rails.application.configure { |c| c.middleware.use RateLimiter, store: Dalli::Client.new }
 end
